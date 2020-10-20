be1ca8378a0ddba9a45ceb668065577a881e597d
Pahud <pahudnet@gmail.com>
2020-10-20 10:30:28 +0800

    update projen upgrade workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,9 @@
 const {
   AwsCdkTypeScriptApp,
-  Semver
+  GithubWorkflow,
 } = require('projen');
+
+const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: "1.65.0",
@@ -10,6 +12,7 @@ const project = new AwsCdkTypeScriptApp({
   authorEmail: "pahudnet@gmail.com",
   repository: "https://github.com/pahud/svs327-reinvent2019.git",
   projenUpgradeSecret: 'AUTOMATION_GITHUB_TOKEN',
+  dependabot: false,
   cdkDependencies: [
     "@aws-cdk/aws-apigateway",
     "@aws-cdk/aws-dynamodb",
@@ -20,6 +23,46 @@ const project = new AwsCdkTypeScriptApp({
     "@aws-cdk/aws-sns-subscriptions",
     "@aws-cdk/aws-sqs",
   ]
+});
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '11 0 * * *'
+  }], // 0:11am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      { uses: 'actions/checkout@v2' },
+      { 
+        uses: 'actions/setup-node@v1',
+        with: {
+          'node-version': '10.17.0',
+        }
+      },
+      { run: `yarn upgrade` },
+      { run: `yarn projen:upgrade` },
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        }
+      },
+    ],
+  },
 });
 
 
